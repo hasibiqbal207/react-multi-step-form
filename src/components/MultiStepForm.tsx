6fd5f3eba792f1aa99cc0efa985d9cfaa1963b2c
hasibiqbal207 <hasibiqbal207@gmail.com>
2024-05-24 03:40:45 +0200

    Progress bar added

---
+++ src/components/MultiStepForm.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import Progress from './Progress';
+
+const message = ["Learn React", "Apply for Jobs", "Invest your income"]
+const MultiStepForm = () => {
+    const [step, setStep] = useState(1);
+    const totalSteps = 3;
+
+    function handlePrevious() {
+        if (step > 1) {
+            setStep(step - 1);
+        }
+    }
+
+    function handleNext() {
+        if (step < 3) {
+            setStep(step + 1);
+        }
+    }
+
+  return (
+    <div className="container">
+        <div className="progress_container">
+            <Progress totalSteps={totalSteps} step={step} className="progress"/>
+            <div className={`${step>=1 ? 'circle active' : 'circle'}`}>1</div>
+            <div className={`${step>=2 ? 'circle active' : 'circle'}`}>2</div>
+            <div className={`${step>=3 ? 'circle active' : 'circle'}`}>3</div>
+        </div>
+        <div className="content">
+            <Message step={step} />
+        </div>
+        <div className="btns">
+            <button className={`${step<=1 ? 'disabled' : 'btn'}`} onClick={handlePrevious}>Previous</button>
+            <button className={`${step>=3 ? 'disabled' : 'btn'}`} onClick={handleNext}>Next</button>
+        </div>
+    </div>
+  )
+}
+
+function Message({ step}) {
+    return <h2>{message[step-1]}</h2>
+}
+
+export default MultiStepForm
